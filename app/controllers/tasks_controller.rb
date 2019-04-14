11ea26747b3d78baed11bb7b78f21591a7d48579
ikaruga <ikaruga777@gmail.com>
2019-04-14 16:21:22 +0900

    更新時の処理を追加する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,12 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to task_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
   private
 
     def task_params
