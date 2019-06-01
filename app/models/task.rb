62b08c89fec04e0925bfb5618de4e27be00fd807
ikaruga <ikaruga777@gmail.com>
2019-06-01 14:32:04 +0900

    コンマが入っていないことを条件にしたい

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,11 @@
 
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  private
+
+    def validate_name_not_including_comma
+      name.include? ','
+    end
 end
